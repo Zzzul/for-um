a49ea79d278e06ed1aebf0ad21f44cd8408687f9
Zzzul <807fahmi@gmail.com>
2021-09-29 12:17:25 +0700

    Fix vote not responsive on mobile :bug:

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -24,55 +24,117 @@
             </div>
         </div>
 
-        <div class="row justify-content-center mt-0">
-            <div class="col-md-1">
-                <div class="card">
-                    <div class="card-body pl-3 text-center">
-                        <form action="{{ route('vote.store') }}" class="d-flex form-inline" method="POST">
-                            @csrf
-                            @method('post')
+        {{-- Desktop --}}
+        <div class="d-none d-md-block">
+            <div class="row justify-content-center mt-0">
+                <div class="col-md-1">
+                    <div class="card">
+                        <div class="card-body pl-3 text-center">
+                            <form action="{{ route('vote.store') }}" class="d-flex form-inline" method="POST">
+                                @csrf
+                                @method('post')
 
-                            <div class="{{ $hasVotes == 'up' ? 'text-primary' : 'text-secondary' }}">
-                                <input type="hidden" name="post_id" value="{{ $post->id }}">
+                                <div class="{{ $hasVotes == 'up' ? 'text-primary' : 'text-secondary' }}">
+                                    <input type="hidden" name="post_id" value="{{ $post->id }}">
 
-                                <input type="hidden" name="type" value="1">
+                                    <input type="hidden" name="type" value="1">
 
-                                <button
-                                    class="btn btn-transparent p-1{{ $hasVotes == 'up' ? ' text-primary' : ' text-secondary' }}"
-                                    type="submit">
-                                    <h5>{{ $post->up_votes_count }}</h5>
-                                    <i class="fas fa-sort-up fa-3x"></i>
-                                </button>
+                                    <button
+                                        class="btn btn-transparent p-1{{ $hasVotes == 'up' ? ' text-primary' : ' text-secondary' }}"
+                                        type="submit">
+                                        <h5>{{ $post->up_votes_count }}</h5>
+                                        <i class="fas fa-sort-up fa-3x"></i>
+                                    </button>
+                                </div>
+                            </form>
+
+
+                            <form action="{{ route('vote.store') }}" class="d-flex form-inline" method="POST">
+                                @csrf
+                                @method('post')
+
+                                <div class="{{ $hasVotes == 'down' ? 'text-primary' : 'text-secondary' }}">
+                                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+
+                                    <input type="hidden" name="type" value="0">
+
+                                    <button
+                                        class="btn btn-transparent p-1{{ $hasVotes == 'down' ? ' text-primary' : ' text-secondary' }}"
+                                        type="submit">
+                                        <i class="fas fa-sort-down fa-3x"></i>
+
+                                        <h5>{{ $post->down_votes_count }}</h5>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-7">
+                    @include('posts.card.post-content', ['post' => $post])
+                </div>
+            </div>
+        </div>
+
+        {{-- Mobile --}}
+        <div class="d-sm-block d-md-none">
+            <div class="row justify-content-center mt-0">
+                <div class="col-md-12">
+                    @include('posts.card.post-content', ['post' => $post])
+                </div>
+
+                <div class="col-md-12 mt-3">
+                    <div class="card">
+                        <div class="card-body py-1 px-5 d-flex justify-content-between">
+                            <div class="ml-5">
+                                <form action="{{ route('vote.store') }}" method="POST">
+                                    @csrf
+                                    @method('post')
+
+                                    <div class="{{ $hasVotes == 'up' ? 'text-primary' : 'text-secondary' }}">
+                                        <input type="hidden" name="post_id" value="{{ $post->id }}">
+
+                                        <input type="hidden" name="type" value="1">
+
+                                        <button
+                                            class="btn btn-transparent p-1{{ $hasVotes == 'up' ? ' text-primary' : ' text-secondary' }}"
+                                            type="submit">
+                                            <h5 class="float-left mr-2 mt-3">{{ $post->up_votes_count }}</h5>
+
+                                            <i class="fas fa-sort-up fa-3x float-right mt-3"></i>
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
 
+                            <div class="mr-5">
+                                <form action="{{ route('vote.store') }}" method="POST">
+                                    @csrf
+                                    @method('post')
 
-                        <form action="{{ route('vote.store') }}" class="d-flex form-inline" method="POST">
-                            @csrf
-                            @method('post')
+                                    <div class="{{ $hasVotes == 'down' ? 'text-primary' : 'text-secondary' }}">
+                                        <input type="hidden" name="post_id" value="{{ $post->id }}">
 
-                            <div class="{{ $hasVotes == 'down' ? 'text-primary' : 'text-secondary' }}">
-                                <input type="hidden" name="post_id" value="{{ $post->id }}">
+                                        <input type="hidden" name="type" value="0">
 
-                                <input type="hidden" name="type" value="0">
+                                        <button
+                                            class="btn btn-transparent p-1{{ $hasVotes == 'down' ? ' text-primary' : ' text-secondary' }}"
+                                            type="submit">
 
-                                <button
-                                    class="btn btn-transparent p-1{{ $hasVotes == 'down' ? ' text-primary' : ' text-secondary' }}"
-                                    type="submit">
-                                    <i class="fas fa-sort-down fa-3x"></i>
+                                            <i class="fas fa-sort-down fa-3x float-left mr-2 mb-3"></i>
 
-                                    <h5>{{ $post->down_votes_count }}</h5>
-                                </button>
+                                            <h5 class="float-right mt-3">{{ $post->down_votes_count }}</h5>
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="col-md-7">
-                @include('posts.card.post-content', ['post' => $post])
-            </div>
         </div>
+
 
         <div class="row justify-content-center">
             <div class="col-md-8">
